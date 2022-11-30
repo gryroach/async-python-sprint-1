b52ce79ecc545483e2705d0d95ac86dd4503622c
Sergey Gavrilenko <gryroach@gmail.com>
2022-11-30 22:59:34 +0300

    5: added tests

--- forecasting.py
+++ forecasting.py
@@ -1,7 +1,4 @@
 import logging
-# import threading
-# import subprocess
-# import multiprocessing
 import time
 
 from api_client import YandexWeatherAPI
@@ -19,7 +16,7 @@ def forecast_weather():
     Анализ погодных условий по городам
     """
     start = time.time()
-    data = DataAggregationTask().aggregate_data()
+    data = DataAggregationTask(CITIES).aggregate_data()
     DataAnalyzingTask(data).analyze_data()
     delta = time.time() - start
     logging.info(f'Время выполнения - {delta:.2f}s')
